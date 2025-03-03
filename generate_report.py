fa0f674de3606895a7e52e1ff2382cc63ccb2f7a
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2025-03-03 02:14:47 -0800

    Create generate_report.py

---
+++ generate_report.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+"""
+    Create Intersight Intersight_reports.xlsx file
+    Creates separate sheets for each component
+    Components:
+        FI, 
+        Chassis, IOM, x-fabric_modules, PCI Nodes
+        PSU, Fan Modules, FANs, 
+        Server, CPU, Memory, Network Adapters, Storage Controllers
+            Physical Drive, Virtual Drive, TPM, PCI Devices
+    Additional Reports:
+    Contract Info
+    FI Disk Usage Info
+    License Info
+    Empty Chassis Slots Info  
+"""
+import os
+import json
+from dotenv import load_dotenv, find_dotenv
+from common import get_token, get_data, parse_data
+from common import write_to_excel, remove_parameters, auto_size_columns
+from common import find_empty_slots, create_hyperlinks_sheet, set_default_sheet
+from common import get_licenses
+
+load_dotenv(find_dotenv())
+
+if __name__ == '__main__':
+    # Set variables
+    client_id = os.getenv("ClientId")
+    client_secret = os.getenv("ClientSecret")
+
+    # Get oAuth Token
+    token = get_token(client_id, client_secret)
+
+    with open('intersight_urls.json', 'r') as f:
+        json_data = json.load(f)
+
+    for k,v in json_data.items():
+        base_path = "https://intersight.com/api/v1/"
+        endpoint_path = v['path']
+        query_parameters = v['query_parameters']
+
+        api_count_url = f"{base_path}{endpoint_path}?$count=True"
+        if query_parameters != "":
+            api_url = f"{base_path}{endpoint_path}?{query_parameters}"
+        else:
+            api_url = f"{base_path}{endpoint_path}"
+  
+
+        # Intersight API Nested Data        
+        data = get_data(client_id, client_secret, token, api_count_url, api_url)
+        
+        if data:
+            # Flattened Data
+            semi_parsed_data = parse_data(data)
+
+            # Remove Parameters from Parsed Data before Writing
+            parsed_data = remove_parameters(semi_parsed_data)
+
+            # Write Flattened Data to a JSON file
+            data_file = f"./{k}.json"
+
+            if k == "Empty_Chassis_Slots":
+                data = find_empty_slots(parsed_data)
+                parsed_data = data
+
+            if k == "licenses":
+                data = get_licenses(parsed_data)
+                parsed_data = data
+            
+            # Create Data json file
+            with open(data_file, 'w') as f:
+                f.write(json.dumps(parsed_data))
+
+            # Create Excel File
+            file_name = "./Intersight_reports.xlsx"   # Update
+            sheet_name = k           # Update
+        
+            print(f"Creating Sheet: {k}")
+            header_list = list(parsed_data[0].keys())
+
+            # for k,v in enumerate(json_data):
+            #     row_data = list(v.values())
+
+            # Write to Elsx file
+            write_to_excel(file_name, sheet_name, header_list, parsed_data)
+
+            # Autofit Columns in sheet
+            auto_size_columns(file_name, sheet_name)
+
+    # Create Hyperlinks Sheet
+    file_name = "./Intersight_reports.xlsx"
+    print(f"Creating Sheet: Hyperlinks")
+    create_hyperlinks_sheet(file_name)
+
+    # Set Hyperlinks as Default Sheet
+    sheet_name = "Hyperlinks"
+    set_default_sheet(file_name, sheet_name)
